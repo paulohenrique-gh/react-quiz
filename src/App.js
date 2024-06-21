028fe18f0dc6eea7b107dc06fcd73d86ca313c90
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-06-21 10:47:12 -0300

    Add main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-import DateCounter from './DateCounter'
+import { useReducer, useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived": {
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+    case "dataFailed": {
+      return { ...state, status: "error" };
+    }
+    default: {
+      throw new Error("Action unknown");
+    }
+  }
+}
+
+// 'loading', 'error', 'ready', 'active', 'finished'
+const initialState = { questions: [], status: "loading" };
 
 export default function App() {
-  return <div>
-    <DateCounter></DateCounter>
-  </div>
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
